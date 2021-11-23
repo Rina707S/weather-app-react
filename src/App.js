710595d014e6bb94e7182b0037163bf5e423e505
Rina707S <88797166+Rina707S@users.noreply.github.com>
2021-11-24 00:03:27 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather project </h1>
-      <p>
-        This app was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/ekaterina-romanenkova-9689b4211/"
-          target="_blank"
-        >
-          Rina R
-        </a>{" "}
-        and it is{" "}
-        <a href="https://github.com/Rina707S/weather-app-react" target="_blank">
-          open-sourced on github
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather project </h1>
+        <footer>
+          This app was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/ekaterina-romanenkova-9689b4211/"
+            target="_blank"
+          >
+            Rina R
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/Rina707S/weather-app-react"
+            target="_blank"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
